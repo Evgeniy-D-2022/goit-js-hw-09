ede7a7b803c0ec35cc76449b0d8278d4ef54fd6e
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-04-24 15:49:44 +0200

    update js_hw_09_02

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+
+refs.startBtn.addEventListener('click', clickStartBtn);
+refs.stopBtn.addEventListener('click', clickStopBtn);
+
+function clickStartBtn() {
+    timerId = setInterval(changeBodyColor, 1000)
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function clickStopBtn() {
+    clearInterval(timerId)
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+}
+
+function changeBodyColor() {
+    let bodyColor = getRandomHexColor();
+    document.body.style.backgroundColor = bodyColor;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,78 @@
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    dateTimePicker: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('button[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+};
+
+let intervalID = null;
+
+refs.startBtn.addEventListener('click', startTimer);
+
+function startTimer() {
+    intervalID = setInterval(endTimer, 1000);
+    refs.startBtn.disabled = true;
+};
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] <= new Date()) {
+            window.alert ('Please choose a date in the future');
+            refs.startBtn.disabled = true;
+        } else {
+            refs.startBtn.disabled = false;
+        };
+    },
+  };
+
+  function endTimer() {
+    const currentDate = new Date();
+    const selectedDate = new Date(refs.dateTimePicker.value);
+    const diff = selectedDate - currentDate;
+    if (diff < 1000) {
+        clearInterval(intervalID);
+        window.alert ('Success')
+    };
+    if (selectedDate !== null) {
+        refs.days.textContent = addLeadingZero(convertMs(diff).days);
+        refs.hours.textContent = addLeadingZero(convertMs(diff).hours);
+        refs.days.textContent = addLeadingZero(convertMs(diff).days);
+        refs.minutes.textContent = addLeadingZero(convertMs(diff).minutes);
+        refs.seconds.textContent = addLeadingZero(convertMs(diff).seconds);
+        
+        refs.startBtn.disabled = true;
+    }
+   return;
+};
+
+
+  const flatPick = flatpickr('input#datetime-picker', options);
+
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  };
+
+  function addLeadingZero(number) {
+    return String(number).padStart(2, 0);
+  };
