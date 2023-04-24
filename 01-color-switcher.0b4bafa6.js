79cb13df1ed79fac8e6223dfe18f09ccc1eb0571
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-04-24 20:49:58 +0000

    Deploying to gh-pages from @ Evgeniy-D-2022/goit-js-hw-09@a955254481f7ee1da0249a5f935803692bb8eb2e 🚀

--- 01-color-switcher.0b4bafa6.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};function e(){let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}t.startBtn.addEventListener("click",(function(){timerId=setInterval(e,1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.0b4bafa6.js.map
