64a4bb3730b54a0847c64ded009d6291abec7b93
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-04-24 22:19:39 +0200

    update hw_js_09_03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+const formEvt = document.querySelector('.form');
+const formData = {};
+
+formEvt.addEventListener('input', formInput);
+formEvt.addEventListener('submit', formSubmit);
+
+function formInput(evt) {
+  formData[evt.target.name] = Number(evt.target.value);
+};
+
+function formSubmit(evt) {
+  evt.preventDefault();
+
+  const { delay, step, amount } = formData;
+
+  if(delay <= 0 || step <= 0 || amount <= 0) {
+    window.alert('Fail!, number must be > 0');
+    console.log('');
+    return;
+  };
+  
+  for (let index = 0; index < amount; index += 1) {
+    createPromise(index + 1, delay + step * index)
+    .then(({position, delay}) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+    });
+  };
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+       resolve({ position, delay })
+      } else {
+       reject({ position, delay })
+      };
+    }, delay);
+  })
 }
